6028cf8137828749ac8434715ff886e3fbf44940
Dmitriy Domostroev <labrokas@gmail.com>
2022-01-20 00:48:57 +0200

    HW 12

--- app.js
+++ app.js
@@ -1,48 +1,78 @@
-/*************************Strings */
-const str1 = "abc";
-const str2 = "abc";
-console.log(`str1 == str2 is ${str1 == str2}`)
-console.log(`"abc" < "ab" is ${str1 < "ab"}`)
-console.log(`"123" > 23 is ${"123" > 23}`);
-console.log(`"123" > "23" is ${"123" > "23"}`);
-/****************************************** */
-//if  logical expression contains both string and number, JS will convert string to number
-//if string doesn't contain a number a result of conversion will be NaN
-//In any logical expression if there is NaN, then the result will be false
-console.log(`"abc" > 23 is ${"abc" > 23}`);
-console.log(`"abc" < 23 is ${"abc" < 23}`);
-console.log(`"abc" != 23 is ${!("abc" == 23)}`)
-/********************************************* */
-function stringProcessing(str) {
-        const strP = "" + str;
-       console.log(`"${str}"[4] is ${str[4]}`);
-       console.log(`length of "${str}" is ${strP.length}`);
-       console.log(`"${str}" includes "abc" is ${strP.includes("abc")}`);
-       console.log(`substring of "${str}" beginning from index 2 to index 5 is ${strP.substring(2, 5)}`);
-       console.log(`index of substring "ll" in the "${str}" is ${strP.indexOf("ll")}; last index of substring "ll" in the "${str}" is  ${strP.lastIndexOf("ll")}`)
+/*************************Arrays */
+const ar = [2, 100, 9, 80];
+// ar.sort(function(a, b){
+//     return a - b; 
+// });
+ar.sort((a,b) => a - b)
+console.log(`ar is ${ar.toString()}`);
+ar[ar.length] = 200;
+console.log(ar);
+ar.push(300, 25);
+console.log(ar);
+const ar1 = [-8, 30, -57];
+//ar.push(ar1); there will be added array but not separated numbers
+//if you want to add separated numbers you should apply a spread operator...
+ar.push(...ar1);
+console.log(ar);
+//for adding elements at begging of an array is method "unshift"
+const ar2 = [27, 35];
+ar.unshift(...ar2);
+console.log(ar);
+ar.splice(2, 0, 1, 2);
+console.log(ar);
+let el = ar.pop();//removes the last element and returns it
+//el = -57; -57 will be removed from the array ar
+el = ar.shift(); //removes the first element and returns it
+//el = 27; 27 will be removed from the array ar
+console.log(el, ar);
+ar.splice(3, 5)//remove 5 elements beginning from the one at index 3
+console.log(ar);
 
+//two dimensional arrays
+const matrix1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [0, 1, 2]];
+const matrix2 = [[10, 20], [50, -5], [34, 28]]
+function displayMatrix(matrix){
+    for(let i = 0; i < matrix.length; i++){
+        let row = '';
+        for(let j = 0; j < matrix[i].length; j++){
+            row = row + matrix[i][j] + '  '
+        }
+        console.log(row);
+    }
 }
-stringProcessing("abcd*lmn*ll(oo&&ttll");
-function encode (num, codingString) {
-    let sym = 1;
-        for (let i = 0; i < codingString.length - 1; i++) {
-            if (codingString[i] == codingString[i + 1]){
-                return console.log("Please choose another codingString.");
-            }
-            sym++;
-        }   
-    let res="";
-        do {
-            const digit = Math.trunc(num % sym);
-            const symb = getSymbol(digit, codingString);
-            res = symb + res;
-            num = Math.trunc(num / sym);
-        } while(num >= 1);
-    return res;
-}
+displayMatrix(matrix1);
+displayMatrix(matrix2);
 
-function getSymbol(digit, codingString) {
-    return "" + codingString[digit];
-}
+//------------------------HW 12 (1)----------------------------
+const arHw = [13, 28, 4, 15, 25, -10, 40, 17,27];
 
-console.log(encode(45, "abcde")); 
+arHw.sort(function(a, b) {
+    if(a % 2 == 0){
+        if(b % 2 == 0){
+            if(a < b) return -1;
+            else if(a > b) return 1;
+            else return 0; 
+        }
+        else return -1;
+    }
+    else{
+        if(b % 2 != 0){
+            if(a < b) return 1;
+            else if (a > b) return -1;
+            else return 0
+        }
+        else return 1;
+    }
+})
+  console.log(arHw);
+
+//------------------------HW 12 (2)----------------------------
+// function displayMatrix3(matrix){
+//     for(let i = 0; i < matrix.length; i++){
+//         for(let j = 0; j < matrix[i].length; j++){
+//         }
+//         console.log(matrix1);
+//     }
+// }
+// const matrix3 = [[1, 2], [4, 5], [7, 8]];
+// displayMatrix3(matrix3)
