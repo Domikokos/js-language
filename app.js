1b401710d33bd44bd3582e725d072a65b7089c9e
Dmitriy Domostroev <labrokas@gmail.com>
2022-01-31 19:10:07 +0200

    final commit

--- app.js
+++ app.js
@@ -1,41 +1,50 @@
-// const ar = [-10, 50, -12, 80, 40];
-// ar.push(70);
-// // str - "-10#50#-12#80#40#70"
-// //******************Solution based on substring */
-// // let str = '';
-// // ar.forEach(function(n){
-// //     str += n + '#';
-// // })
-// // str = str.substring(0, str.length - 1);
-// // const lastSharpIndex = str.lastIndexOf('#');
-// // const str1 = str.substring(0, lastSharpIndex);
-// // const str2 = str.substring(lastSharpIndex + 1);
-// // str = str1 + str2;
-// //******************Solution based on forEach form second number */
-// // const ar1 = ar.slice(1);
-// // let str = '' + ar[0];
-// // ar1.forEach(n => str += '#' + n);
-// // console.log(str);
-// //*******************Printing out sequense number of element, element, length of array */
-// //1 of 5 - 10;   ....
-// ar.forEach((n, i, a) => console.log(`${i + 1} of ${a.length} - ${n}`));
+const person1 = {id: 123, name: 'Moshe', adress: {city: 'Lod', street: 'Sokolov'}};
+const person2 = {id: 123, name: 'Moshe', adress: {city: 'Lod', street: 'Sokolov'}};
+const person3 = person1;
+console.log(`"123" == 123 is ${"123" == 123}`);
+console.log(`"123" === 123 is ${"123" === 123}`);
+console.log(`person1 == person3 is ${person1 == person3}`);
+console.log(`person1 == person2 is ${person1 == person2}`);
+console.log(`JSON.stringify(person1) === JSON.stringify(person2) is ${JSON.stringify(person1) === JSON.stringify(person2)}`);
+console.log(JSON.stringify(person1));
+console.log(`Name of person1 is ${person1.name}`);
+console.log(`Person1 lives in city ${person1.adress.city}`);
+Object.keys(person1).forEach(k => console.log(k));//array of the object keys
+Object.values(person1).forEach(v => console.log(v));//array of the object values
+Object.entries(person1).forEach(e => console.log(e));//array of arrays - [key, value]
+function createAdress(city, street){
+    return {city, street}
+}
+function createPerson(id, name, adress){
+    return {id, name, adress}
+}
+const persons = [
+    createPerson(123, "Vasy", createAdress("Rehovot", "Parshani")),
+    createPerson(124, "Olya", createAdress("Rehovot", "Pr. Plaut")),
+    createPerson(125, "Tolya", createAdress("Tel-Aviv", "Dizingoff")),
+]
+function displayOccurrences(array) {
+    //creating object with key - unique element of array (string as an element of array)
+    //                     value - occurrences count
+    //difference between  obj ={a: 123, d: "abc"}; const a = "d" ; obj.a === 123,
+    // and obj[a] === "abc"
+    //obj.c = 10 -> {a: 123, d: "abc", c: 10}
+    const res = {};
+    for (let i = 0; i < array.length; i++) {
+            if (res[array[i]] === undefined) {
+                    //string as content of array[i] occures first time
+                    res[array[i]] = 1;
+            } else {
+                    res[array[i]] = res[array[i]] + 1;
+            }
+    }
+    // console.log(res) -> intermediate debug log
+    Object.entries(res).sort((e1, e2) => {
+            const res = e2[1] - e1[1];
+            return res === 0 ? e1[0].localeCompare(e2[0]) :  res;
+    }).forEach(e => console.log(`${e[0]} -> ${e[1]}`))
 
-// //****************************************** */
-// // method "map"
-// //use case of applying method map : you want to create new array with elements that are received as result of some conversion
-// //example you want to get new array with elements that are muliplication on 2 of each source element
-// const ar2 = ar.map(n => n * 2);
-// console.log(ar2);
-// someFunction();
 
-const ar20 = [13, 17, 20, 23, 2, 40];
-const arEvenOdd = ar20.filter((n, i, a) => a.length % 2 == 0 ? n % 2 == 0 : n % 2 == 1);
-console.log(arEvenOdd);
-
-const res = ar20.reduce((res, cur) => res + cur, 0);
-console.log(res);
-const max = ar20.reduce((max, cur) => cur > max )
-console.log(max);
-console.log(res);
-res = ar20.reduce((res, cur) => res + cur);
-
+}
+const ar = ["bc", "lmn", "d", "d", "lmn", "a", "lmn", "a"];
+displayOccurrences(ar);
